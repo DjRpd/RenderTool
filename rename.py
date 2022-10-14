c11fd4587aa0dc01a705b59c126177d48047326c
intert0uch <info@intertouch.it>
2022-10-14 14:25:59 +0200

    Update rename.py

--- rename.py
+++ rename.py
@@ -5,3 +5,8 @@ for o in pm.ls(selection=True):
 for shp in pm.listRelatives(o, children=True, sh=True):
 if pm.nodeType(shp, q=True)=='mesh':
 '''
+
+#seleziono una cosa alla volta
+
+for shp in pm.listRelatives(o, children=True, sh=True):
+if pm.nodeType(shp, q=True)=='mesh':
